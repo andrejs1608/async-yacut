b000786dc5c1279c5ce1a3e07990e2c062587a41
andrejs1608 <andrjuha1608@gmail.com>
2026-01-31 19:05:25 +0100

    м

--- settings.py
+++ settings.py
@@ -2,7 +2,10 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB', 'sqlite:///db.sqlite3')
+    QLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI',
+        os.getenv('DB', 'sqlite:///db.sqlite3')
+    )
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-key-for-dev')
     API_HOST = 'https://cloud-api.yandex.net/'
 
